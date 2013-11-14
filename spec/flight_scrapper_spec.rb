189fbf632db1c9d810aa370f5e58f2ceb1505d58
martiserra <marti@Marti.local>
2013-11-14 21:45:00 +0100

    added respond_to #search test for every supplier

--- spec/flight_scrapper_spec.rb
+++ spec/flight_scrapper_spec.rb
@@ -2,12 +2,24 @@ require 'spec_helper'
 
 describe FlightScrapper do
 	
+	before :all do
+		@available_suppliers = FlightScrapper.get_suppliers
+	end
+
 	it '#get_suppliers should provide U2 supplier' do
 
 		@available_suppliers = FlightScrapper.get_suppliers
 		@available_suppliers.should be_a Hash
 		@available_suppliers.should have_key 'U2'
+
+	end
+
+	it 'every supplier should respond_to #search' do
 		
+		@available_suppliers.each_value { |supplier|
+			supplier.should respond_to :search
+		}
+	
 	end
 
 end
