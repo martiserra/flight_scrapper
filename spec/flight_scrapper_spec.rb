5984ab8ec53013df5dc2b934c28a4ae5798fb9cb
martiserra <marti.serra.alabau@gmail.com>
2013-11-28 22:12:06 +0100

    removed funky stuff from flight_scrapper test

--- spec/flight_scrapper_spec.rb
+++ spec/flight_scrapper_spec.rb
@@ -8,7 +8,6 @@ describe FlightScrapper do
 
 	it '#get_suppliers should provide U2 supplier' do
 
-		@available_suppliers = FlightScrapper.get_suppliers
 		@available_suppliers.should be_a Hash
 		@available_suppliers.should have_key 'U2'
 
