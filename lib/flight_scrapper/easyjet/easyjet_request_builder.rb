d6fdab45e4670eed44cc3f09e659c3d73dba5366
martiserra <marti.serra.alabau@gmail.com>
2013-11-28 22:32:02 +0100

    implemented one way search URL request building for U2

--- lib/flight_scrapper/easyjet/easyjet_request_builder.rb
+++ lib/flight_scrapper/easyjet/easyjet_request_builder.rb
@@ -11,16 +11,26 @@ module FlightScrapper
 			url += 'dep=' + search_criteria.origin
 			url += '&dest=' + search_criteria.destination
 			url += '&dd=' + search_criteria.outbound_date.strftime('%d/%m/%Y')
-			url += '&rd=' + search_criteria.inbound_date.strftime('%d/%m/%Y')
+			url += get_return_date(search_criteria.inbound_date)
 			url += '&apax=' + search_criteria.adults.to_s
 			url += '&pid=www.easyjet.com'
 			url += '&cpax=' + search_criteria.childs.to_s
 			url += '&ipax=' + search_criteria.infants.to_s
 			url += '&lang=EN'
-			url += '&isOneWay=off'
+			url += get_is_one_way(search_criteria.inbound_date)
 			url += '&searchFrom=SearchPod|/en/'
 
 			url
+		end
+
+		def self.get_is_one_way inbound_date
+			is_one_way = '&isOneWay='
+			is_one_way += inbound_date.nil? ? 'on' : 'off'
+			is_one_way
+		end
+
+		def self.get_return_date inbound_date
+			inbound_date.nil? ? '' : '&rd=' + inbound_date.strftime('%d/%m/%Y')
 		end
 
 	end
