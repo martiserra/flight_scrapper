fd06f1eaa6330a4d5f40c87dd82c5247943d7e50
martiserra <marti.serra.alabau@gmail.com>
2013-11-28 22:19:23 +0100

    added test for easyjet OneWay search url building

--- spec/easyjet/easyjet_request_builder_spec.rb
+++ spec/easyjet/easyjet_request_builder_spec.rb
@@ -9,6 +9,11 @@ module FlightScrapper
 				criteria = FactoryGirl.build(:search_criteria, :round_trip_one_adult)
 				EasyjetRequestBuilder.build_url(criteria).should eql 'http://www.easyjet.com/links.mvc?dep=BCN&dest=LTN&dd=18/01/2014&rd=04/04/2014&apax=1&pid=www.easyjet.com&cpax=0&ipax=0&lang=EN&isOneWay=off&searchFrom=SearchPod|/en/'
 			end
+
+			it 'builds the OneWay URL' do
+				criteria = FactoryGirl.build(:search_criteria, :one_way_all_passenger_types)
+				EasyjetRequestBuilder.build_url(criteria).should eql 'http://www.easyjet.com/links.mvc?dep=BCN&dest=LTN&dd=18/1/2014&apax=1&pid=www.easyjet.com&cpax=1&ipax=1&lang=EN&isOneWay=on&searchFrom=SearchPod|/en/'
+			end
 		
 		end
 	end
