5e0e3d24f3024980f7f148614fa57f414c5f15a1
martiserra <marti.serra.alabau@gmail.com>
2013-11-28 22:11:17 +0100

    added factory for rt search criteria

---
+++ spec/factories.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+  factory :search_criteria do
+    
+    trait :round_trip_one_adult do
+      ignore do
+        origin 'BCN'
+        destination 'LTN'
+        outbound_date Date.new(2014,1,18) 
+        inbound_date Date.new(2014,4,4) 
+        adults 1
+        childs 0
+        infants 0
+      end
+
+      initialize_with { SearchCriteria.new(origin, destination, outbound_date, inbound_date, adults, childs, infants) }
+    end
+  
+  end
+end
