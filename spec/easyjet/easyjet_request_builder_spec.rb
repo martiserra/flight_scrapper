5d0dbf07ad026772e18e563484329efa42db51f8
martiserra <marti.serra.alabau@gmail.com>
2013-11-28 22:12:36 +0100

    testing for EasyjetRequestBuilder round trip

---
+++ spec/easyjet/easyjet_request_builder_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+module FlightScrapper
+
+	describe EasyjetRequestBuilder do
+		describe '#build_url' do
+			
+			it 'builds the RoundTrip URL' do
+				criteria = FactoryGirl.build(:search_criteria, :round_trip_one_adult)
+				EasyjetRequestBuilder.build_url(criteria).should eql 'http://www.easyjet.com/links.mvc?dep=BCN&dest=LTN&dd=18/01/2014&rd=04/04/2014&apax=1&pid=www.easyjet.com&cpax=0&ipax=0&lang=EN&isOneWay=off&searchFrom=SearchPod|/en/'
+			end
+		
+		end
+	end
+
+end
